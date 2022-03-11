840b819331b333a52fedb302c2f790c1f80320a0
Peter Andrews <pmandrews@gmail.com>
2022-03-11 16:27:57 -0500

    Tweak main page to avoid too much confusion

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,10 +1,6 @@
 <template>
   <q-page class="flex flex-center">
-    <img
-      alt="Quasar logo"
-      src="~assets/quasar-logo-vertical.svg"
-      style="width: 200px; height: 200px"
-    >
+    <p>Hi! Quote Your Friends is under development and not ready for public use yet. Check back later!</p>
   </q-page>
 </template>
 
