17fabbe9e6fb908e11fa389c9f386f13990e8baf
Peter Andrews <pmandrews@gmail.com>
2022-03-11 11:27:26 -0500

    Use real name in header bar

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -12,10 +12,8 @@
         />
 
         <q-toolbar-title>
-          Quasar App
+          Quote Your Friends
         </q-toolbar-title>
-
-        <div>Quasar v{{ $q.version }}</div>
       </q-toolbar>
     </q-header>
 
