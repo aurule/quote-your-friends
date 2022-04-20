623e6eeb0e7e27b953474f4b1f5ad79d014d8f62
Peter Andrews <pmandrews@gmail.com>
2022-04-20 10:52:25 -0400

    Add database configs

---
+++ config/db.js
@@ -0,0 +1,54 @@
+require("dotenv").config()
+
+const { logger } = require("../src/util/logger")
+
+const global_options = {
+  logging: (msg) => logger.debug(msg)
+}
+
+module.exports = {
+  global_options,
+  development: {
+    ...global_options,
+    username: "root",
+    password: null,
+    database: "development",
+    dialect: "sqlite",
+    storage: ".sqlite/development.sqlite",
+    transactionType: "IMMEDIATE",
+  },
+  test: {
+    ...global_options,
+    username: "root",
+    password: null,
+    database: "test",
+    dialect: "sqlite",
+    storage: ".sqlite/test.sqlite",
+  //   username: "postgres",
+  //   password: "postgres",
+  //   database: "postgres",
+  //   host: "localhost",
+  //   port: 5433,
+  //   dialect: "postgres",
+    transactionType: "IMMEDIATE",
+  },
+  ci: {
+    ...global_options,
+    username: "postgres",
+    password: "postgres",
+    database: "postgres",
+    host: "postgres",
+    port: 5432,
+    dialect: "postgres",
+    transactionType: "IMMEDIATE",
+  },
+  production: {
+    ...global_options,
+    username: "postgres",
+    password: process.env.DB_PW,
+    database: "postgres",
+    host: process.env.DB_HOST,
+    port: 5432,
+    dialect: "postgres",
+  }
+}
